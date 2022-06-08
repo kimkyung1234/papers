8bf403f03bb53152ee5b38b68351f2939cf0b0a4
kimkyungwon <openwind125@gmail.com>
2022-06-08 19:38:49 +0900

    Add - PhotosList model 추가

---
+++ lib/models/photos_list_model.dart
@@ -0,0 +1,16 @@
+import 'package:papers/models/photo_model.dart';
+
+class PhotosList {
+  PhotosList({
+    this.photos,
+  });
+
+  List<Photo>? photos;
+
+  factory PhotosList.fromJson(List<dynamic> parsedJson) {
+    List<Photo> photos = [];
+    photos = parsedJson.map((i) => Photo.fromJson(i)).toList();
+
+    return PhotosList(photos: photos);
+  }
+}
