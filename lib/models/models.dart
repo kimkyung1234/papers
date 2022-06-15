ed3892671fa01b78f3f625e5332881359d092cc5
kimkyungwon <openwind125@gmail.com>
2022-06-15 16:43:09 +0900

    Refactoring - 파일 구조 분리

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,8 +1,8 @@
-export 'exif_model.dart';
-export 'links_model.dart';
-export 'location_model.dart';
-export 'photo_model.dart';
+export 'photo_info_models/exif_model.dart';
+export 'photo_info_models/links_model.dart';
+export 'photo_info_models/location_model.dart';
+export 'photo_info_models/photo_model.dart';
 export 'photos_list_model.dart';
 export 'search_result_model.dart';
-export 'urls_model.dart';
+export 'photo_info_models/urls_model.dart';
 export 'user_model.dart';
