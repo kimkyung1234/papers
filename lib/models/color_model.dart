b5fe652563ac5ba6d321487f33dbabe61b4a8c64
kimkyungwon <openwind125@gmail.com>
2022-06-16 18:08:13 +0900

    Add - Color 모델 추가

---
+++ lib/models/color_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  ColorTheme({
+    this.backgroundColor,
+    this.textColor,
+    this.underTextColor,
+    this.cardColor,
+  });
+
+  Color? backgroundColor;
+  Color? textColor;
+  Color? underTextColor;
+  Color? cardColor;
+}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -4,6 +4,7 @@ export 'photo_info_models/links_model.dart';
 export 'photo_info_models/location_model.dart';
 export 'photo_info_models/photo_model.dart';
 export 'collection_list_model.dart';
+export 'color_model.dart';
 export 'photos_list_model.dart';
 export 'search_result_model.dart';
 export 'photo_info_models/urls_model.dart';
